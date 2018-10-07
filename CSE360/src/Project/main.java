39acb4253d30e9c5004de514ec129ce5ec7881f5
racso1518 <oscar.amaya.115@gmail.com>
2018-10-07 12:53:34 -0700

    kjnk

--- CSE360/src/Project/main.java
+++ CSE360/src/Project/main.java
@@ -1,10 +1,14 @@
 package Project;
 
+import javax.swing.*;
+
+import java.util.*;
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		UserInterface user = new UserInterface();
+		
 	}
 
 }
